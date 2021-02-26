9fa4d3b4e5b971b6556f19b123bf996835d37061
jo <ljonas@riseup.net>
2021-02-26 12:31:34 +0100

    Format build.sh script

--- build.sh
+++ build.sh
@@ -5,8 +5,8 @@ script="matrix.js"
 
 line_number=$(grep -n 'script: |' "$src" | cut -d ':' -f1)
 
-head -n "$line_number" "$src" > "$src.tmp" # Keep head of the file
+head -n "$line_number" "$src" > "$src.tmp"                # Keep head of the file
 sed -r -e 's/^(\S*)/        \1/g' "$script" >> "$src.tmp" # Insert script
-sed -i 's/[[:space:]]*$//' "$src.tmp" # Remove trailling spaces
+sed -i 's/[[:space:]]*$//' "$src.tmp"                     # Remove trailling spaces
 
 mv "$src.tmp" "$src"

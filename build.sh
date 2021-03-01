24a7c0387f31dd628988afdd9d62514519aad4d0
jo <ljonas@riseup.net>
2021-03-01 21:22:49 +0100

    Add codespell pre-commit hook and fix spellin

--- build.sh
+++ build.sh
@@ -7,6 +7,6 @@ line_number=$(grep -n 'script: |' "$src" | cut -d ':' -f1)
 
 head -n "$line_number" "$src" > "$src.tmp"                # Keep head of the file
 sed -r -e 's/^(\S*)/        \1/g' "$script" >> "$src.tmp" # Insert script
-sed -i 's/[[:space:]]*$//' "$src.tmp"                     # Remove trailling spaces
+sed -i 's/[[:space:]]*$//' "$src.tmp"                     # Remove trailing spaces
 
 mv "$src.tmp" "$src"
